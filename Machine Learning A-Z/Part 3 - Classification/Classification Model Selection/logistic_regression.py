344f5aca083a34704f03a81cbe53028451bdb037
ashna-khemani <ashna.khemani@gmail.com>
2023-02-27 08:34:18 -0500

    print accuracy scores
    Was not printing

--- Machine Learning A-Z/Part 3 - Classification/Classification Model Selection/logistic_regression.py
+++ Machine Learning A-Z/Part 3 - Classification/Classification Model Selection/logistic_regression.py
@@ -29,4 +29,4 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+print(accuracy_score(y_test, y_pred))

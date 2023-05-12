7c70cd2e9acab7974c5cc93271a451289469fbe6
ashna-khemani <ashna.khemani@gmail.com>
2023-05-11 20:16:48 -0700

    Random Forest Classification

--- Machine Learning A-Z/Part 3 - Classification/Section 20 - Random Forest Classification/my_file.R
+++ Machine Learning A-Z/Part 3 - Classification/Section 20 - Random Forest Classification/my_file.R
@@ -33,7 +33,7 @@ y_pred = predict(classifier, newdata = test_set[-3])
 cm = table(test_set[, 3], y_pred)
 
 # Visualising the Training set results
-#library(ElemStatLearn)
+library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
@@ -49,7 +49,7 @@ points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualising the Test set results
-#library(ElemStatLearn)
+library(ElemStatLearn)
 set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
